629474de429951b341070893f1761c742f432422
Kleber Tarcísio <klebertarcisio@yahoo.com.br>
2020-04-22 14:01:48 -0300

    teste vulnerabilidade
    teste vulnerabilidade

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 int main(){
+	#ifdef MACRO
+		// uma fraqueza
+		char str1[] = "uma parte da string," + "outra parte.";
+	#else
+		// mesma fraqueza	
+		char str1[] = "uma parte da string," + "outra parte.";
+	#endif
 	return 0;
 }
-
